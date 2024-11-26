50c2dd144e27680a46aa33f9c79b252f9735c1b1
Suresh M <sureshkbm90@gmail.com>
2024-11-26 15:07:11 +0530

    Added home screen

--- CryptoCoin/CryptoCoin/ViewController.swift
+++ CryptoCoin/CryptoCoin/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let viewModel = ViewModel()
+    
+    override func loadView() {
+        super.loadView()
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        view.backgroundColor = .white
+        self.view = view
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "Coin"
     }
 
 
---
+++ CryptoCoin/CryptoCoin/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  CryptoCoin
+//
+//  Created by Suresh M on 26/11/24.
+//
+
+import Foundation
+
+class ViewModel {
+    
+}
